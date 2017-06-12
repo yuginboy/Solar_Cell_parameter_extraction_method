50285bb30d7e0ebc6a59b6f1dc7505a9c89b0f36
yuginboy <preved*NIX2016>
2017-06-12 17:46:33 +0200

    next

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ def fun_for_minimization(b, Volts, Voc_inp, Isc_inp):
     Rs = b[1]
     Rsh = b[2]
 
+    rnd_uniq = 2*np.random.random(size=Volts.size) - 1
+    y_noise = 0.01* Isc_inp * rnd_uniq
+
     N = np.size(Volts)
     Ical = np.zeros(N)
     i = 0
@@ -31,34 +34,43 @@ def fun_for_minimization(b, Volts, Voc_inp, Isc_inp):
         # arg = q*Rs/(n*kB*T)*(Isc - Voc/(Rs+Rsh))*np.exp(-q*Voc/(n*kB*T))* np.exp(q*(Rs*Isc+Rsh*V/(Rs+Rsh))/(n*kB*T))
         # Ical[i] = n*kB*T/(q*Rs)*lambertw(arg) + V/Rs - Isc - Rsh*V/(Rs*(Rs+Rsh))
 
-        arg1 = Rsh*( Rs*( (Isc+(Rs*Isc - Voc)/Rsh)/(1 - np.exp(q*(Rs*Isc - Voc)/(n*kB*T))) ) + Rs*Voc/Rsh + V )
+        # =============================
+        # =============================
 
-        arg2 = arg1/ (Rs*(Rs+Rsh))
+        # arg1 = Rsh*( Rs*( (Isc+(Rs*Isc - Voc)/Rsh)/(1 - np.exp(q*(Rs*Isc - Voc)/(n*kB*T))) ) + Rs*Voc/Rsh + V )
+        #
+        # arg2 = arg1/ (Rs*(Rs+Rsh))
+        #
+        # argLambert1 = (q*Rs/(n*kB*T)*(Isc - Voc/(Rs+Rsh))*np.exp(-q*Voc/(n*kB*T)) ) / \
+        #              (1 - np.exp(q*(Rs*Isc - Voc)/(n*kB*T)))
+        #
+        # argLambert2 = np.exp( Rsh*q*( Rs*( (Isc+(Rs*Isc - Voc)/Rsh)/(1 - np.exp(q*(Rs*Isc - Voc)/(n*kB*T))) ) + Rs*Voc/Rsh + V ) /
+        #                       (n*kB*T*(Rs + Rsh)) )
+        #
+        # LambertFunc = lambertw(argLambert1*argLambert2)
+        #
+        # arg3 = n*kB*T/q/Rs*LambertFunc
+        #
+        # Ical[i] = V/Rs -  arg2 + arg3
+        #
+        # =============================
 
-        argLambert1 = (q*Rs/(n*kB*T)*(Isc - Voc/(Rs+Rsh))*np.exp(-q*Voc/(n*kB*T)) ) / \
-                     (1 - np.exp(q*(Rs*Isc - Voc)/(n*kB*T)))
+        Ical[i] = -(
+            -q * V + (-lambertw(
+            q * b[1] * (Isc - (Voc - b[1] * Isc) / b[2]) * np.exp(-q * Voc / (b[0] * kB * T)) * b[2] /
+            (b[1] * b[0] * kB * T + b[2] * b[0] * kB * T) * np.exp(b[2] * q * (b[1] * (Isc + b[1] * Isc / b[2]) + V)
+                                                                / b[0] / kB / T / (b[2] + b[1]))) +
+            b[2] * q * (b[1] * (Isc + b[1] * Isc / b[2]) + V) / b[0] / kB / T / (b[2] + b[1])) * b[0] * kB * T
+                    ) / q / b[1]
 
-        argLambert2 = np.exp( Rsh*q*( Rs*( (Isc+(Rs*Isc - Voc)/Rsh)/(1 - np.exp(q*(Rs*Isc - Voc)/(n*kB*T))) ) + Rs*Voc/Rsh + V ) /
-                              (n*kB*T*(Rs + Rsh)) )
 
-        LambertFunc = lambertw(argLambert1*argLambert2)
-
-        arg3 = n*kB*T/q/Rs*LambertFunc
-
-        Ical[i] = 0*V/Rs -  arg2 + 0*arg3
-
-        # Ical[i] = -(
-        #     -q * V + (-lambertw(
-        #     q * b[1] * (Isc - (Voc - b[1] * Isc) / b[2]) * np.exp(-q * Voc / (b[0] * kB * T)) * b[2] /
-        #     (b[1] * b[0] * kB * T + b[2] * b[0] * kB * T) * np.exp(b[2] * q * (b[1] * (Isc + b[1] * Isc / b[2]) + V)
-        #                                                         / b[0] / kB / T / (b[2] + b[1]))) +
-        #     b[2] * q * (b[1] * (Isc + b[1] * Isc / b[2]) + V) / b[0] / kB / T / (b[2] + b[1])) * b[0] * kB * T
-        #             ) / q / b[1]
         i = i+1
-    return Ical
+    return -Ical + y_noise
 
 def func(b, I, V, Voc_inp, Isc_inp):
     return I - fun_for_minimization(b, V, Voc_inp, Isc_inp)
+
+
 
 def func_abs(b, I, V, Voc_inp, Isc_inp):
     return np.sum(np.abs(fun_for_minimization(b, V, Voc_inp, Isc_inp) - I))
@@ -69,9 +81,10 @@ if __name__ == '__main__':
     print('-> you run ', __file__, ' file in a main mode')
 
 
-    data = np.loadtxt('/home/yugin/VirtualboxShare/Giorgi.Tchutchulashvili/src/data/Dark_and_Ilum_AUPD1.dat')
+    # data = np.loadtxt('/home/yugin/VirtualboxShare/Giorgi.Tchutchulashvili/src/data/Dark_and_Ilum_AUPD1.dat')
+    data = np.loadtxt('/home/yugin/VirtualboxShare/Giorgi.Tchutchulashvili/src/data/reference_Si.dat')
     x = data[:, 0]
-    y = data[:, 1]
+    y = data[:, 1]*100/1e4
     fig, ax = plt.subplots(figsize=(7, 5))
     ax.plot(x, y, label='experiment')
     ax = plt.gca()
@@ -93,6 +106,7 @@ if __name__ == '__main__':
     Voc = x0
     Isc = y0
     args = (x, y, Voc, Isc)
+    args_cf = (Voc, Isc)
 
     ax.plot(x0, 0.0, 'o', color = 'r', label='Voc={:1.3e}'.format(Voc))
     ax.plot(0.0, y0, 'o', color = 'b', label='Isc={:1.3e}'.format(Isc))
@@ -105,31 +119,53 @@ if __name__ == '__main__':
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
 
+    # ==================================
 
-    # b0 = np.asarray([1.7, 1000, 1000], dtype=float)
-    # # bounds = [(1, 2), (0, np.inf), (0, np.inf)]
-    # bounds = ([1, 0.1, 0.1], [2, 1e12, 1e12])
+
+    b0 = np.asarray([1.65, 1.2e4, 0.684e7], dtype=float) # for refe Si sample
+    # b0 = np.asarray([1.5, 1.0e4, 0.684e6], dtype=float)
+    # bounds = [(1, 2), (0, np.inf), (0, np.inf)]
+    bounds = ([1, 1e2, 1e2], [2, 1e8, 1e8])
     # res = least_squares(func, b0, bounds=bounds, args=args, gtol=1e-12)
-    # print(res)
-    # b = res.x
-    # print('===' * 15)
-    # print('least_sq:' * 15)
-    # print('n = {0:1.6f}, Rs0 = {1:1.6f}, Rsh0 = {2:1.6f}'.format(b[0], b[1], b[2]))
-    # ax.plot(x, (fun_for_minimization(res.x, Volts=x, Voc_inp=Voc, Isc_inp=Isc)), '-g', label='LS')
-    # plt.legend()
 
-    bounds = [(1, 2), (0, 1e5), (0, 1e6)]
-    # bounds = ([1, 0.1, 0.1], [2, np.inf, np.inf])
+    def func_cf(I, a, b, c, Voc_inp=Voc, Isc_inp=Isc):
+        # for curve_fit procedure:
+        p0 = np.array([a, b, c])
+        out = I - fun_for_minimization(p0, y, Voc_inp, Isc_inp)
+        return out
 
-    result = differential_evolution(func_abs, bounds=bounds, args=args, disp=True, tol=1e-8)
-
-    b = result.x
+    popt, pcov = curve_fit(func_cf, xdata=x, ydata=y, p0=b0, bounds=bounds, absolute_sigma=True)
+    print(popt)
+    print(pcov)
+    b = popt.data
     print('===' * 15)
-    print('de:' * 15)
+    print('least_sq:' * 15)
     print('n = {0:1.6f}, Rs0 = {1:1.6f}, Rsh0 = {2:1.6f}'.format(b[0], b[1], b[2]))
+    ax.plot(x, (fun_for_minimization(b, Volts=x, Voc_inp=Voc, Isc_inp=Isc)), '-g', label='CF')
+    plt.legend()
 
-    # b = np.asarray([1.5, 10000, 1000], dtype=float)
-    ax.plot(x, (fun_for_minimization(b, Volts=x, Voc_inp=Voc, Isc_inp=Isc)), '-r', label='DE')
+
+
+    # ==================================
+    #
+    # bounds = [(1, 2), (1e3, 1e8), (1e3, 1e8)]
+    # # bounds = ([1, 0.1, 0.1], [2, np.inf, np.inf])
+    #
+    # result = differential_evolution(func_abs, bounds=bounds, args=args, disp=False, tol=1e-11, maxiter=int(1e4), strategy='randtobest1exp',)
+    #
+    # b = result.x
+    # print('===' * 15)
+    # print('de:' * 15)
+    # print('n = {0:1.6f}, Rs0 = {1:1.6f}, Rsh0 = {2:1.6f}'.format(b[0], b[1], b[2]))
+    #
+    # # b = np.asarray([1.5, 10000, 1000], dtype=float)
+    # ax.plot(x, (fun_for_minimization(b, Volts=x, Voc_inp=Voc, Isc_inp=Isc)), '-r', label='DE')
+
+    # ==================================
+
+
+
+
     plt.legend()
     # ax.set_xlim(xlim)
     # ax.set_ylim(ylim)
